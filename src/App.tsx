e40d81f0cdb9758daf68554b2bee485b6596c4e1
Shaun Saker <sakershaun@gmail.com>
2022-12-26 13:50:58 +1000

    add globalStyles

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
+import { CustomScrollbar } from "./components/customScrollbar/CustomScrollbar";
+import { GlobalStyles } from "./components/globalStyles/GlobalStyles";
 import { ThemeProvider } from "./components/themeProvider/ThemeProvider";
 import { Router } from "./Router";
 
 export const App = () => {
   return (
     <ThemeProvider>
+      <GlobalStyles />
+
+      <CustomScrollbar />
+
       <Router />
     </ThemeProvider>
   );
