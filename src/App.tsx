9d2742032a0fed00fabcab73513b63d09e54459f
Shaun Saker <sakershaun@gmail.com>
2022-12-26 13:15:49 +1000

    basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { Router } from "./Router";
+
 export const App = () => {
-  return <div />;
+  return <Router />;
 };
---
+++ src/Router.tsx
@@ -0,0 +1,19 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Error } from "./pages/error/Error";
+import { Home } from "./pages/home/Home";
+
+export enum Routes {
+  home = "/",
+}
+
+const router = createBrowserRouter([
+  {
+    path: Routes.home,
+    element: <Home />,
+    errorElement: <Error />,
+  },
+]);
+
+export const Router = () => {
+  return <RouterProvider router={router} />;
+};
